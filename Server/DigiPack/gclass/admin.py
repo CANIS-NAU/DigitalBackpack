b086ac598b7c42759b5cd1ee93df80ef65cc7586
ashiqrobin <ashiq.robin@gmail.com>
2020-12-21 13:53:40 -0600

    loginapp updated

--- Server/DigiPack/gclass/admin.py
+++ Server/DigiPack/gclass/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import student_info
 
 # Register your models here.
+@admin.register(student_info)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['student_roll','student_name']
