afc81c17291b600ccec9c7190920ec8e095228e3
ashiqrobin <ashiq.robin@gmail.com>
2021-01-19 12:24:22 -0600

    classapi model update

--- DigiServer/DigiBackend/classapi/models.py
+++ DigiServer/DigiBackend/classapi/models.py
@@ -1,3 +1,55 @@
+import os
+import pickle
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Greeting(models.Model):
+    when = models.DateTimeField("date created", auto_now_add=True)
+
+
+class Classroom(models.Model):
+
+    service = None
+
+    def get_token(self):
+        """ The file token.pickle stores the user's access and refresh tokens,
+        and is created automatically when the authorization flow completes
+        for the first time"""
+        creds = None
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+                return creds
+
+    def save_token(self, credentials):
+        """Save the credentials for next run"""
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(credentials, token)
+
+    def login(self):
+        """If there are no (valid) credentials available, let the user log in"""
+        creds = self.get_token()
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', settings.GOOGLE_API_SCOPES)
+                creds = flow.run_local_server(port=0)
+                self.save_token(credentials=creds)
+                self.service = build('classroom', 'v1', credentials=creds)
+
+    def list_courses(self):
+        results = self.service.courses().list().execute()
+        return results.get('courses', [])
+
+
+
+class Course(models.Model):
+    id = models.CharField(max_length=12, primary_key=True)
+    name = models.CharField('Name', max_length=60)
+
+    def __str__(self):
+        return self.name
