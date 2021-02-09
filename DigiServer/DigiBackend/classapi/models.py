1c410b5ee959084f0522e79b037a542539a27b2f
ashiqrobin <ashiq.robin@gmail.com>
2021-02-09 12:32:56 -0600

    application and model update

--- DigiServer/DigiBackend/classapi/models.py
+++ DigiServer/DigiBackend/classapi/models.py
@@ -6,50 +6,48 @@ from googleapiclient.discovery import build
 from django.db import models
 from django.conf import settings
 
-class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
 
+# class Classroom(models.Model):
 
-class Classroom(models.Model):
+#     service = None
 
-    service = None
+#     def get_token(self):
+#         """ The file token.pickle stores the user's access and refresh tokens,
+#         and is created automatically when the authorization flow completes
+#         for the first time"""
+#         creds = None
+#         if os.path.exists('token.pickle'):
+#             with open('token.pickle', 'rb') as token:
+#                 creds = pickle.load(token)
+#                 return creds
 
-    def get_token(self):
-        """ The file token.pickle stores the user's access and refresh tokens,
-        and is created automatically when the authorization flow completes
-        for the first time"""
-        creds = None
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
-                creds = pickle.load(token)
-                return creds
+#     def save_token(self, credentials):
+#         """Save the credentials for next run"""
+#         with open('token.pickle', 'wb') as token:
+#             pickle.dump(credentials, token)
 
-    def save_token(self, credentials):
-        """Save the credentials for next run"""
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(credentials, token)
+#     def login(self):
+#         """If there are no (valid) credentials available, let the user log in"""
+#         creds = self.get_token()
+#         if not creds or not creds.valid:
+#             if creds and creds.expired and creds.refresh_token:
+#                 creds.refresh(Request())
+#             else:
+#                 flow = InstalledAppFlow.from_client_secrets_file('credentials.json', settings.GOOGLE_API_SCOPES)
+#                 creds = flow.run_local_server(port=0)
+#                 self.save_token(credentials=creds)
+#                 self.service = build('classroom', 'v1', credentials=creds)
 
-    def login(self):
-        """If there are no (valid) credentials available, let the user log in"""
-        creds = self.get_token()
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', settings.GOOGLE_API_SCOPES)
-                creds = flow.run_local_server(port=0)
-                self.save_token(credentials=creds)
-                self.service = build('classroom', 'v1', credentials=creds)
-
-    def list_courses(self):
-        results = self.service.courses().list().execute()
-        return results.get('courses', [])
+#     def list_courses(self):
+#         results = self.service.courses().list().execute()
+#         return results.get('courses', [])
 
 
 
 class Course(models.Model):
-    id = models.CharField(max_length=12, primary_key=True)
-    name = models.CharField('Name', max_length=60)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    course_name = models.CharField('Course Name', max_length=200)
+    course_grade = models.CharField('Course Grade', max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.user.email
--- DigiServer/DigiBackend/classapi/views.py
+++ DigiServer/DigiBackend/classapi/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+import pickle
+import os.path
+import hashlib
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/classroom.rosters',
+'https://www.googleapis.com/auth/classroom.courses.readonly',
+#'https://www.googleapis.com/auth/classroom.coursework.me.readonly',
+#'https://www.googleapis.com/auth/classroom.coursework.students.readonly',
+'https://www.googleapis.com/auth/classroom.announcements',
+]
+
+def login(user_id):
+    user_bytes = user_id.encode('utf-8')
+    user = hashlib.sha256(user_bytes).hexdigest()
+    creds = None
+    if os.path.exists(f'static/tokens/{user}.pickle'):
+        with open(f'static/tokens/{user}.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(f'static/tokens/{user}.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('classroom', 'v1', credentials=creds)
+
+    return service
+
+    # Call the Classroom API
+
+def course_details(final_service):
+    results = final_service.courses().list().execute()
+    courses = results.get('courses', [])
+
+    if not courses:
+        print('No courses found.')
+    else:
+        print('Courses:')
+        for course in courses:
+            #print(course)
+            print(course['name'])
+            print('Section: ',course['section'])
+            print('Course State: ',course['courseState'])
+            #print(course['userId'])
+
+@api_view(['GET','POST'])
+def get_studentclassinfo(request):
+    pass
+
+@api_view(['GET','POST'])
+def get_subjectinfo(request):
+    pass
+
+@api_view(['GET','POST'])
+def get_annoucement(request):
+    pass
+
